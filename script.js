facdad101fbb7885ea44b868662227fdb49965bf
19261rs <69171712+19261rs@users.noreply.github.com>
2023-10-16 06:29:46 +0000

    V23: Collecting User First Name & Surname Functionality

--- script.js
+++ script.js
@@ -88,16 +88,17 @@ setTimeout(() => {
 //RENTAL FORM CODE STARTS HERE 
 
 // Arrays
-const CARSEATSARRAY = [8,7,5,4,2,1];
-const CARTYPEARRAY = ["Estima", "Outlander", "Falcon", "Mini", "MX5", "BAC Mono"];
+const TRUCKWEIGHTARRAY = [8,7,5,4,2,1];
+const TRUCKTYPEARRAY = ["Estima", "Outlander", "Falcon", "Mini", "MX5", "BAC Mono"];
 
 //Variables
-var userName; 
-var userSeatChoice;
+var userFirstName; 
+var userLastName; 
+var userWeightChoice;
 var userAge;
 var userHireLength; 
 var userCost;
-var userCarChoice;
+var userTruckChoice;
 var userInsurance;
 var userInsuranceResp;
 
@@ -105,8 +106,8 @@ var userInsuranceResp;
 const FLATRATE = 10;
 const MINAGE = 25;
 const MAXAGE = 80;
-const MINSEATS = 1; 
-const MAXSEATS = 8;
+const MINWEIGHT = 1; 
+const MAXWEIGHT = 8;
 const INSURANCERATE = 20;
 const MAXRENTDAYS = 14;
 const MINRENTDAYS = 0;
@@ -114,7 +115,7 @@ const MINRENTDAYS = 0;
 //Validation variables (Booleans)
 var nameValidation = true;
 var ageValidation = true;
-var seatValidation = true;
+var Validation = true;
 var daysValidation = true;
 var insuranceValidation = true;
 
@@ -124,123 +125,25 @@ var insuranceValidation = true;
 
 function start() {
   //HTML user input values 
-  userName = document.getElementById("i_name").value; 
-  userAge = document.getElementById("i_age").value;
-  userSeatChoice = document.getElementById("i_seats").value; 
-  userHireLength = document.getElementById("i_days").value;
-  userInsuranceResp = document.getElementById("i_insurance").value;
+  userFirstName = document.getElementById("firstName").value; 
+  userLastName = document.getElementById("lastName").value;
+  // userAge = document.getElementById("age").value;
+ 
 
   // Console log HTML values (testing purposes)
-  console.log(userName);
-  console.log(userAge);
-  console.log(userSeatChoice);
-  console.log(userHireLength);
-  console.log(userInsuranceResp);
- 
-  // Name validation 
-  const regExpName = /^[a-zA-Z]+$/;
-  var userName = userName.trim();
-  if (regExpName.test(userName) == true) {
-    // If name input is valid
-    var nameValidation = true;
-  } 
-  
-  //Age validation 
-  const regExpAge = /^[0-9]+$/;
-  var userAge = userAge.trim();
-  console.log(regExpAge.test(userAge))
-  if (regExpAge.test(userAge) == true && userAge >= MINAGE && userAge <= MAXAGE){
-  // If age input is valid
-    var ageValidation = true;
-  }
+  console.log(userFirstName);
+  console.log(userLastName);
+  // console.log(userAge);
+  // console.log(userSeatChoice);
+  // console.log(userHireLength);
+  // console.log(userInsuranceResp);
 
-  //Hire Length validation 
-  const regExpHire = /^[0-9]+$/;
-  var userHireLength = userHireLength.trim();
-  console.log(regExpHire.test(userHireLength))
-  if (regExpHire.test(userHireLength) == true && userHireLength >MINRENTDAYS && userHireLength <= MAXRENTDAYS){
-    // If days input is valid
-    var daysValidation = true;
-  } 
-  
-  //Seat Selection validation
-  const regExpSeat = /^[0-9]+$/;
-  var userSeatChoice = userSeatChoice.trim();
-  console.log(regExpSeat.test(userSeatChoice))
-  if (regExpSeat.test(userSeatChoice) == true && userSeatChoice  >= MINSEATS && userSeatChoice <= MAXSEATS && userSeatChoice != 3 && userSeatChoice !=6) {
-   // If seats input is valid
-    var seatValidation = true;
-  }
-
-  // Insurance validation
-  const regExpInsure = /^(?:Yes\b|No\b)+$/i;
-  var userInsuranceResp = userInsuranceResp.trim();
-  console.log(regExpInsure.test(userInsuranceResp))
-  if (regExpInsure.test(userInsuranceResp) == true) {
-    // if insurance input is valid
-    var insuranceValidation = true;
-  } 
-
-  // Checking validation and continuing if true, else invalid message
-  if (nameValidation == true && ageValidation == true && daysValidation == true && seatValidation == true && insuranceValidation == true) {
-    carSelection();
-  } else {
-    validationFailed();
-  }
+  alert("Hello " + userFirstName + userLastName);
+}
 /**********************************************************************************/
 // Functions
 /**********************************************************************************/
 
-// Matching vehicle based off number of car seats
-  function carSelection() {
-    for (i = 0; i < CARSEATSARRAY.length; i++) {
-      if (userSeatChoice >= CARSEATSARRAY[i]) { 
-          userCarChoice = CARTYPEARRAY[i]
-          userSeatChoice = CARSEATSARRAY [i]
-          console.log(userCarChoice);
-          console.log(userSeatChoice);
-          break;
-    }
-  }
-  
-//Cost Calculation 
-if (userInsuranceResp == 'Yes'|| userInsuranceResp == 'yes'|| userInsuranceResp == 'YES') {
-   //calculate insurance
-  userInsurance = Number(userHireLength * INSURANCERATE);
-  //final cost including insurance
-  userCost = Number(userHireLength * userSeatChoice * FLATRATE + userInsurance);
-  console.log(userCost);
-} else {
-   userCost = Number(userHireLength * userSeatChoice * FLATRATE);
-   console.log(userCost);
-}
-  
-// Recommendation prompt & Final order message
-//local variable - doesn't need to be called universely at the top.
-var userConfirmResp = confirm("We recommend that the " + userCarChoice + " is the best vehicle for you! It is equipped with " + userSeatChoice + " seats, making it the best option.\nPlease confirm if you'd like the " + userCarChoice + ".");
-
-if (userConfirmResp == true){
-    document.getElementById('u_response').innerHTML = "YOUR ORDER: Hello " + userName + " you've chosen the " +  userCarChoice + ". It comes with " + userSeatChoice +  " seats and it will cost $" + userCost + " to hire for " + userHireLength + " day(s).";
-} else {
- document.getElementById('u_response').innerHTML = "Your order has been canceled.";
-}
-}
-
-// Failed validation function 
-function validationFailed() {
-    if (nameValidation !==true) {
-      document.getElementById('u_response').innerHTML = "Please enter a valid name!";
-    } else if (ageValidation !==true) {
-       document.getElementById('u_response').innerHTML = "Please enter a valid age! You must be between 25-80 years old to hire a vehicle.";
-    } else if (seatValidation !==true){
-      document.getElementById('u_response').innerHTML = "Sorry, it looks like we don't have a vehicle with that many seats available!";
-    } else if(daysValidation !==true){
-      document.getElementById('u_response').innerHTML = "Please enter valid number of days you'd like to hire for!"; 
-    } else if (insuranceValidation !==true) {
-      document.getElementById('u_response').innerHTML = "Invalid! Please make sure you've selected either 'Yes' or 'No' for insurance!"; 
-    }
- }
-}
 /*************************************************************/
 //      END OF SHERGILL TRANSPORT APP
 /*************************************************************/
